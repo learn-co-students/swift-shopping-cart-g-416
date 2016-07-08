cfc295fa9d83b7df294f1fd1ad6ce10cdd4ad2cf
HaarisMuneer <hom9@cornell.edu>
2016-07-08 15:42:45 -0400

    update pods

--- Podfile
+++ Podfile
@@ -4,8 +4,8 @@
 use_frameworks!
 
 def testing_pods
-  pod 'Quick', '~> 0.8.0'
-  pod 'Nimble', '~> 3.0.0'
+  pod 'Quick'
+  pod 'Nimble'
 end
 
 target 'swift-shopping-cart' do
---
+++ Pods/Target Support Files/Pods-swift-shopping-cart/Pods-swift-shopping-cart-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_swift_shopping_cartVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_swift_shopping_cartVersionString[];
+
