bf4add8966e62e487add2b49ad866dd13c09d961
JimCampagno <jimcampagno@gmail.com>
2016-09-27 18:11:10 -0400

    Remove Solution

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,61 +7,6 @@
 //
 
 class Cart {
-    var items = [Item]()
     
-    func totalPriceInCents() -> Int {
-        var total = 0
-        
-        for item in items {
-            total += item.priceInCents
-        }
-        
-        return total
-    }
     
-    func add(item: Item) {
-        items.append(item)
-    }
-    
-    func remove(item: Item) {
-        if let index = items.index(of: item) {
-            items.remove(at: index)
-        }
-    }
-    
-    func items(withName name: String) -> [Item] {
-        var matches = [Item]()
-        
-        for item in items {
-            if item.name == name {
-                matches.append(item)
-            }
-        }
-        
-        return matches
-    }
-    
-    func items(withMinPrice price: Int) -> [Item] {
-        var matches = [Item]()
-        
-        for item in items {
-            if item.priceInCents >= price {
-                matches.append(item)
-            }
-        }
-        
-        return matches
-    }
-    
-    func items(withMaxPrice price: Int) -> [Item] {
-        var matches = [Item]()
-        
-        for item in items {
-            if item.priceInCents <= price {
-                matches.append(item)
-            }
-        }
-        
-        return matches
-    }
 }
